6f1a478e2ca317ee8cf2276b6956c8071762eb0b
Humphrey Hu <humphrey_hu@163.com>
2022-03-16 17:33:40 +0800

    新增插入选择排序、插入排序和希尔排序

---
+++ leetcode/src/com/hhp/pointers/LeetCodeCompress.java
@@ -0,0 +1,62 @@
+package com.hhp.pointers;
+
+public class LeetCodeCompress {
+
+    public int anotherCompress(char[] chars) {
+        int n = chars.length, write = 0;
+        for (int i = 0; i < n; ) {
+            int j = i;
+            while (j < n && chars[i] == chars[j]) {
+                j++;
+            }
+            chars[write++] = chars[i];
+            if (j - i > 1) {
+                String tmp = Integer.toString(j - i);
+                for (int k = 0; k < tmp.length(); k++) {
+                    chars[write++] = tmp.charAt(k);
+                }
+            }
+            i = j;
+        }
+        return write;
+    }
+
+    public int compress(char[] chars) {
+        int left = 0, right = 0;
+        int len = chars.length;
+
+        while (right < len) {
+            int index = right;
+            while (right < len && chars[index] == chars[right]) {
+                right++;
+            }
+
+            int repeats = right - index;
+            chars[left] = chars[right - 1];
+
+            if (repeats == 1) {
+                left++;
+            } else if (repeats <= 9) {
+                chars[left + 1] = (char)(repeats + '0');
+                left += 2;
+            } else {
+                String num = repeats + "";
+                for (int i = 0; i < num.length(); i++) {
+                    chars[left + 1 + i] = num.charAt(i);
+                }
+                left += num.length() + 1;
+            }
+        }
+        return left;
+    }
+
+    public static void main(String[] args) {
+        char[] chars = new char[]{
+                'a', 'a', 'a',
+                'b', 'b',
+                'c', 'c', 'c'
+        };
+        LeetCodeCompress codeCompress = new LeetCodeCompress();
+        System.out.println(codeCompress.compress(chars));
+    }
+}
---
+++ sort/src/com/hhp/mergesort/MergeSort.java
@@ -0,0 +1,46 @@
+package com.hhp.mergesort;
+
+import java.util.Arrays;
+
+public class MergeSort {
+
+    public int[] mergeSort(int[] sourceArr) {
+        int[] arr = Arrays.copyOf(sourceArr, sourceArr.length);
+
+        if (arr.length < 2) {
+            return arr;
+        }
+        int middle = (int) Math.floor(arr.length / 2);
+
+        int[] left = Arrays.copyOfRange(arr, 0, middle);
+        int[] right = Arrays.copyOfRange(arr, middle, arr.length);
+
+        return merge(mergeSort(left), mergeSort(right));
+    }
+
+    private int[] merge(int[] left, int[] right) {
+        int[] result = new int[left.length + right.length];
+        int i = 0;
+        while (left.length > 0 && right.length > 0) {
+            if (left[0] <= right[0]) {
+                result[i++] = left[0];
+                left = Arrays.copyOfRange(left, 1, left.length);
+            } else {
+                result[i++] = right[0];
+                right = Arrays.copyOfRange(right, 1, right.length);
+            }
+        }
+
+        while (left.length > 0) {
+            result[i++] = left[0];
+            left = Arrays.copyOfRange(left, 1, left.length);
+        }
+
+        while (right.length > 0) {
+            result[i++] = right[0];
+            right = Arrays.copyOfRange(right, 1, right.length);
+        }
+
+        return result;
+    }
+}
