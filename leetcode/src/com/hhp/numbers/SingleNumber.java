a00111e29f2f5617cd3976c0d4b19169d8a52772
Humphrey Hu <humphrey_hu@163.com>
2022-03-19 16:52:35 +0800

    补充注释

--- leetcode/src/com/hhp/numbers/SingleNumber.java
+++ leetcode/src/com/hhp/numbers/SingleNumber.java
@@ -2,6 +2,13 @@ package com.hhp.numbers;
 
 public class SingleNumber {
 
+    /**
+     * 异或的运算性质
+     * 1. a ^ 0 = a;
+     * 2. a ^ a = 0;
+     * 3. 交换律：a ^ b = b ^ a
+     * 4. 结合律：(a ^ b) ^ c = a ^ (b ^ c)
+     * */
     public int singleNumber(int[] nums) {
         int single = 0;
         for (int num : nums) {
